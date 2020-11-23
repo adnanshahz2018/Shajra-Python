14d3422254599138e039bb2aec734953c3acccce
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-11-23 14:14:29 +0500

    Adding New Nodes by "Name"

--- shajra.py
+++ shajra.py
@@ -1,5 +1,6 @@
 #   Shajra Application 
  
+
 class Node:
     def __init__(self, name, parent):
         self.__name = name 
@@ -21,11 +22,23 @@ class Node:
     def get_child(self):
         return self.__child
 
+ALPHA_NODE = Node('Deen', None)
+
 class tree:
     space_length = 0
+    desired_node = None
     
+    def set_desired_node(self):
+        self.desired_node = None
+    
+    def add_by_name(self, name, parent_name):
+        self.set_desired_node()
+        self.search_desired_node(parent_name, ALPHA_NODE)
+        self.add_new_node(name, self.desired_node)
+
     def add_new_node(self, name, parent):
         node = None
+        print('\n PARENT === ', parent.get_name())
         if parent is None:
             print('Parent is None aganist -> ', name)
         else:
@@ -33,14 +46,24 @@ class tree:
             parent.update_child(node)
         return node
     
-    def display_vertical(self, node):
+    def display_vertical(self, name):
+        self.set_desired_node()
+        self.search_desired_node(name, ALPHA_NODE)
+        self.vertical_display(self.desired_node)
+        
+    def vertical_display(self, node):    
         if node is None:
             return
-        self.display_vertical(node.parent)
-        print(node.name)
+        self.vertical_display(node.get_parent())
+        print(node.get_name())
         print('  |  ')
 
-    def display_horizontal(self, node):
+    def display_horizontal(self, name):
+        self.set_desired_node()
+        self.search_desired_node(name, ALPHA_NODE)
+        self.horizontal_display(self.desired_node)
+
+    def horizontal_display(self, node):
         if node.get_parent() is None:
             print('\nSorry No Data Found\n')
             return
@@ -49,8 +72,12 @@ class tree:
             for child in node.get_child():
                 print (child.get_name(), ' | ', end="")
 
-    def complete_traversal(self, node):
-        # print(node.get_name())
+    def complete_traversal(self, name):
+        self.set_desired_node()
+        self.search_desired_node(name, ALPHA_NODE)
+        self.traverse(self.desired_node)
+
+    def traverse(self, node):
         if node is None:
             return
         else:
@@ -62,8 +89,21 @@ class tree:
                 print(node.get_name())
                 self.space_length += 1
                 for child in node.get_child():
-                    # self.format_tree()
-                    self.complete_traversal(child)
+                    self.traverse(child)
+                self.space_length -= 1
+
+
+    def search_desired_node(self, name, node):
+        if node is None:
+            print('\nsearch node in none \n')
+            return
+        elif node.get_name() == name:
+            self.desired_node = node
+        else:
+            if len(node.get_child()) != 0:
+                self.space_length += 1
+                for child in node.get_child():
+                    self.search_desired_node(name, child)
                 self.space_length -= 1
 
     def format_tree(self):
@@ -71,30 +111,63 @@ class tree:
             print('  |', end="")
 
 
+def save_family_tree_data_in_text_file():
+    save_data(ALPHA_NODE)
+   
+def save_data(node):
+    if node is None:
+        return
+    else:
+        with open('shajra.txt', 'a+') as f:
+            if node.get_parent() is not None:
+                f.write(node.get_name() + ',' + node.get_parent().get_name() + '\n')
+            else:    
+                f.write(node.get_name() + ',' + 'None' + '\n')
+        if len(node.get_child()) == 0:
+           pass
+        else:
+            for child in node.get_child():
+                save_data(child)
+
+
 if __name__ == "__main__":
     print('\tShajra Python Application\n')
-    f1 = Node('Deen ', None)
-
+    f1 = ALPHA_NODE
     T = tree()
-    f2 = T.add_new_node('Akbar', f1) 
-    f20 = T.add_new_node('Bashir', f1) 
+
+    with open('shajra.txt', 'r') as f:
+        f.readline()
+        lines = f.readlines()
+        for line in lines:
+            names = line.split(',')
+            T.add_by_name(names[0], names[1])
+            print(names[0], ' -- ',  names[1])
+
+
+    # f2 = T.add_new_node('Akbar', f1) 
+    # f20 = T.add_new_node('Bashir', f1) 
     
-    f3 = T.add_new_node('Ihsan', f2) 
+    # f3 = T.add_new_node('Ihsan', f2) 
 
-    f31 = T.add_new_node('Inam', f2) 
-    f310 = T.add_new_node('Shahzeb', f31) 
-    f311 = T.add_new_node('Danial', f31) 
-    f312 = T.add_new_node('Ali', f31) 
+    # f31 = T.add_new_node('Inam', f2) 
+    # f310 = T.add_new_node('Shahzeb', f31) 
+    # f311 = T.add_new_node('Danial', f31) 
+    # f312 = T.add_new_node('Ali', f31) 
 
-    f32 = T.add_new_node('Anwar', f2) 
-    f320 = T.add_new_node('Jazib', f32) 
+    # f32 = T.add_new_node('Anwar', f2) 
+    # f320 = T.add_new_node('Jazib', f32) 
     
-    f40 = T.add_new_node('Rizwan', f3)
-    f41 = T.add_new_node('Adnan', f3) 
-    f42 = T.add_new_node('Anzil', f3) 
+    # f40 = T.add_new_node('Rizwan', f3)
+    # f41 = T.add_new_node('Adnan', f3) 
+    # f42 = T.add_new_node('Anzil', f3) 
 
-    # T.display_vertical(f40)
-    # T.display_horizontal(f40)
-    T.complete_traversal(f1)
-    print('\n\n')
+    name = 'Adnan'
+
+    # T.display_vertical(name)
+    # T.display_horizontal(name)
+
+    # T.complete_traversal(name)
+
+    # save_family_tree_data_in_text_file()
+    print('\n')
 
